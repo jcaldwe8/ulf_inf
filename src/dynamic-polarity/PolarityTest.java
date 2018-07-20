a6fe96eaf37a0df5256b23f0f3e9ad47fc82d453
Gene Kim <gkim21@cs.rochester.edu>
2018-07-20 03:59:12 -0400

    Added file interface to PolarityTest.java and updated run_natlog.sh accordingly.

--- src/dynamic-polarity/PolarityTest.java
+++ src/dynamic-polarity/PolarityTest.java
@@ -6,6 +6,12 @@ import edu.stanford.nlp.util.StringUtils;
 import edu.stanford.nlp.naturalli.Polarity;
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -23,31 +29,69 @@ public class PolarityTest {
     setProperty("natlog.neQuantifiers", "true");
   }});
 
-  public static Polarity[] annotate(String text) {
+  public static List<CoreLabel> annotate(String text) {
     Annotation ann = new Annotation(text);
     pipeline.annotate(ann);
     List<CoreLabel> tokens = ann.get(CoreAnnotations.SentencesAnnotation.class).get(0).get(CoreAnnotations.TokensAnnotation.class);
-    Polarity[] polarities = new Polarity[tokens.size()];
-    for (int i = 0; i < tokens.size(); ++i) {
-      polarities[i] = tokens.get(i).get(NaturalLogicAnnotations.PolarityAnnotation.class);
-    }
-    return polarities;
+    return tokens;
   }
 
 	public static void main(String[] args) {
-    Scanner snr = new Scanner(System.in);
-    while (true) {
-      System.out.println("Write sentence for annotation ('q' to quit): ");
-      String str = snr.nextLine();
-
-      if (str.trim().equals("q")) {
-        return;
+    if (args.length == 0) {
+      // Run an interactive loop if no arguments are provided.
+      Scanner snr = new Scanner(System.in);
+      while (true) {
+        System.out.println("Write sentence for annotation ('q' to quit): ");
+        String str = snr.nextLine();
+  
+        if (str.trim().equals("q")) {
+          return;
+        }
+  
+        List<CoreLabel> tokens = annotate(str);
+        String[] tokenWords = new String[tokens.size()];
+        Polarity[] polarities = new Polarity[tokens.size()];
+        for (int i = 0; i < tokens.size(); ++i) {
+          tokenWords[i] = tokens.get(i).word();
+          polarities[i] = tokens.get(i).get(NaturalLogicAnnotations.PolarityAnnotation.class);
+        }
+        String wordResult = StringUtils.join(Arrays.stream(tokenWords).collect(Collectors.toList()), " ");
+        String polarityResult = StringUtils.join(Arrays.stream(polarities).map(Polarity::toString).collect(Collectors.toList()), " ");
+        
+        System.out.println(wordResult);
+        System.out.println(polarityResult);
       }
+    } else if (args.length == 2) {
+      // Read the file given in the first argument and write to file in the second argument. 
+      try {
+        System.out.println("Reading sentence from " + args[0]);
+        System.out.println("Writing annotations to " + args[1]);
+        BufferedReader br = new BufferedReader(new FileReader(args[0]));
+        String line = br.readLine();  // Assumes a single line.
+        br.close();
 
-      Polarity[] polarities = annotate(str);
-      String result = StringUtils.join(Arrays.stream(polarities).map(Polarity::toString).collect(Collectors.toList()), " ");
-      System.out.println(result);
+        List<CoreLabel> tokens = annotate(line);
+        String[] tokenWords = new String[tokens.size()];
+        Polarity[] polarities = new Polarity[tokens.size()];
+        for (int i = 0; i < tokens.size(); ++i) {
+          tokenWords[i] = tokens.get(i).word();
+          polarities[i] = tokens.get(i).get(NaturalLogicAnnotations.PolarityAnnotation.class);
+        }
+        String wordResult = StringUtils.join(Arrays.stream(tokenWords).collect(Collectors.toList()), " ");
+        String polarityResult = StringUtils.join(Arrays.stream(polarities).map(Polarity::toString).collect(Collectors.toList()), " ");
+        
+        BufferedWriter bw = new BufferedWriter(new FileWriter(args[1]));
+        bw.write(wordResult);
+        bw.newLine();
+        bw.write(polarityResult);
+        bw.close();
+      
+      } catch (Exception e) {
+        System.out.println("Error reading/writing file.");
+      }
+    } else {
+      System.out.println("Invalid number of arguments.");
     }
-	}
+  }
 }
 
--- src/dynamic-polarity/run_natlog.sh
+++ src/dynamic-polarity/run_natlog.sh
@@ -1,4 +1,8 @@
-# Script to run NatLog from CoreNLP.
+#!/bin/bash
+
+# Script to get NatLog polarity annotations from CoreNLP.
+# Up-to-date Stanford CoreNLP jars can be downloaded from https://stanfordnlp.github.io/CoreNLP/index.html#download.
+# This only requires the primary corenlp jar and the corenlp models.
 
 #CP=".:~/research/CoreNLP/stanford-corenlp.jar:~/research/CoreNLP/lib:~/research/CoreNLP/liblocal:stanford-corenlp-models-current.jar:stanford-english-corenlp-models-current.jar:stanford-english-kbp-corenlp-models-current.jar:stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1.jar"
 
@@ -8,4 +12,11 @@ CORENLP_VERSION="3.9.1"
 CP=".:${JAR_PATH}/stanford-corenlp-${CORENLP_VERSION}.jar:${JAR_PATH}/stanford-corenlp-${CORENLP_VERSION}-models.jar"
 
 javac -cp $CP  PolarityTest.java
-java -cp $CP PolarityTest
+
+if [ $# -eq 0 ]; then
+  java -cp $CP PolarityTest
+elif [ $# -eq 2 ]; then
+  java -cp $CP PolarityTest $1 $2 
+else
+  echo "Invalid number of arguments."
+fi
