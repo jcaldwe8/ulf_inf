e3e5f080fe4950e0b251b612bffaeb4d72a78622
bkane2 <Ben.kane@me.com>
2018-07-31 19:17:13 -0400

    Added implicative rules and dynamic polarity to init

--- src/init.lisp
+++ src/init.lisp
@@ -8,12 +8,19 @@
 (load "util-from-pilot-project.lisp")
 (load "ttt-preds-and-functions.lisp")
 
+;; Dynamic Polarity
+(ql:quickload :cl-strings)
+(ql:quickload :alexandria)
+(load "dynamic-polarity/dynamic-polarity.lisp")
+
 ;; Core inference code.
 (load "inference-core.lisp")
 
 ;; Pheomenon-specific code.
 (load "counterfactual-inferences.lisp")
 (load "request-inferences.lisp")
+(load "implicative-inferences.lisp")
+(load "implicative-weak-inferences.lisp")
 
 ;; Top-level inference code.
 (load "inference.lisp")
