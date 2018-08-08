c4ba0a37511d5b7cd045548aa512538a26eac54f
bkane2 <Ben.kane@me.com>
2018-08-08 19:31:29 -0400

    Added test case for negative polarity

--- src/test/implicative-unit-tests.lisp
+++ src/test/implicative-unit-tests.lisp
@@ -8,19 +8,9 @@
 (defun apply-implicative-rules (ulf)
   (ttt:apply-rules
     (mapcar (lambda (x) (if (equalp (slot-value x 'polarity)
-      (let ((seg (segment-ulf ulf)))
+      (let ((seg (get-ulf-segments-vp ulf)))
         (get-segment-polarity (first seg) (second seg) (third seg))))
     (slot-value x 'rule))) *infer-from-implicative-rules*) ulf :shallow t))
-
-;; Function to get ulf segments necessary for dynamic polarity
-;; NOTE: needs to be implemented (currently using static example which returns positive polarity)
-(defun segment-ulf (ulf)
-  (let*
-    ((ulfp1 '((PRES KNOW.V) (THAT (|MARY| ((PAST BE.V) COLD.A)))))
-    (ulfp2 '(THE.D MAN.N))
-    (compulf (list ulfp2 ulfp1)))
-  ;; (segment parent fullulf)
-  (list ulfp1 compulf compulf)))
 
 
 
@@ -34,8 +24,12 @@
   "Testing basic functionality of implicative inferences"
   (:tag :basic :implicative-inferences)
   (assert-equal
-    '(|Mary| ((past be.v) cold.a))
+    '(| Mary| ((past be.v) cold.a))
       (apply-implicative-rules
-    '((the.d man.n) ((pres know.v) (that (|Mary| ((past be.v) cold.a)))))))
+    '((the.d man.n) ((pres know.v) (that (| Mary| ((past be.v) cold.a)))))))
+  (assert-equal
+    '(| Mary| ((past be.v) cold.a))
+      (apply-implicative-rules
+    '((the.d man.n) ((past do.aux-s) not (know.v (that (| Mary| ((past be.v) cold.a))))))))
   ;; Define more here...
   )
