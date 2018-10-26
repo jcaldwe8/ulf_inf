b0f2f66ea3f7bb53be700d483ed72fe26f2859ca
Gene Kim <gkim21@cs.rochester.edu>
2018-10-25 22:54:03 -0400

    Added inference annotation formatting/filtering.

---
+++ src/test/oct-2018-inference-eval/combine-and-format-inf-anns.py
@@ -0,0 +1,87 @@
+"""
+Gene Kim 10-25-2018
+Combines and formats the inferene annotations into a file for running the experiments.
+
+The input file is of the following format:
+  isid\tinfid\tuserid\tinference\tinf-category\tinf_type\ttemporal-reln\ttimestamp
+where the first line is the header. 
+
+
+
+1. Check inference coverage
+2. Map all situational inferences to the recent format.
+3. Remove NO INFERENCES FOR CATEGORY entries
+"""
+
+import json
+import sys
+import os
+
+if len(sys.argv) < 3:
+  sys.exit("ERROR(usage): python combine-and-format-ulf-anns.py [inf ann file] [formatted inf ann file]")
+
+# File that pulls from SQL to get ulf inference id info.
+SID_SENT_ULF_FILE = sys.argv[1]
+OUTFILE = sys.argv[2]
+
+rawfile = file(SID_SENT_ULF_FILE, 'r').read()
+parsed_infile = [l.split("\t") for l in rawfile.split(os.linesep)[1:] if l.strip() != ""]
+
+# 1. Group by isid and check for coverage in every sentence.
+REQUIRED_INF_CATEGORIES = set(["cf", "rq", "q", "impl"])
+isid2infs = {}
+for pline in parsed_infile:
+  isid = pline[0]
+  if isid not in isid2infs:
+    isid2infs[isid] = []
+  isid2infs[isid].append(pline)
+cats_missing = []
+for isid, plines in isid2infs.iteritems():
+  pline_cats = set([pline[4] for pline in plines])
+  if len(pline_cats.intersection(REQUIRED_INF_CATEGORIES)) != \
+      len(REQUIRED_INF_CATEGORIES):
+    cats_missing.append((isid, plines))
+    #print pline_cats
+    #sys.exit("{} categories not annotated for isid {}.".format(
+    #  str(REQUIRED_INF_CATEGORIES.difference(pline_cats)), isid))
+
+if len(cats_missing) > 0:
+  sys.exit("cats missing in following isids: ({})".format(
+    ",".join([isid for isid, pl in cats_missing])))
+
+
+# 2. Map situational inferences to the recent format. 
+
+# Old situational annotation contains "situational" in inf_type.
+def is_oldsit_line(pline):
+  return "situational" in pline[5]
+
+newsit_isid2infs = {}
+for isid, plines in isid2infs.iteritems():
+  newplines = []
+  for pline in plines:
+    if is_oldsit_line(pline):
+      pline[3] = "sit"
+      pline[4] = "situational"
+      newplines.append(pline)
+    else:
+      newplines.append(pline)
+  newsit_isid2infs[isid] = newplines
+
+# 3. Remove NO INFERENCES FOR THIS CATEGORY INFERENCES.
+def is_noinf_line(pline):
+  return "not-" in pline[5]  
+
+outlines = []
+for isid, plines in newsit_isid2infs.iteritems():
+  for pline in plines:
+    if not is_noinf_line(pline):
+      outlines.append(pline)
+    #if is_noinf_line(plines[len(plines) - i - 1]):
+    #  del plines[len(plines) - i - 1]
+
+out = file(OUTFILE, 'w')
+out.write("isid\tinfid\tuserid\tinf_text\tinf_category\tinf_type\ttemporal_reln\ttimestamp\n")
+out.write("\n".join(["\t".join(l) for l in outlines]))
+out.close()
+
--- src/test/oct-2018-inference-eval/get_annotations.sh
+++ src/test/oct-2018-inference-eval/get_annotations.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set +e
+
 # Gene Kim 10-22-2018
 # Retrieves ULF annotations from the SQL database with a given set of sentence 
 # ids and formats it for the inference experiments.  Currently what this 
@@ -20,20 +22,24 @@ DEFAULT_INF_ANN_FILE=${DEFAULT_ANN_DIR}"assigned_inf_anns.txt"
 DATASET_JSON_FILE="../../../oct-2018-inference-subset/source_dataset.json"
 
 # Generated files.
-ULF_SQL_REQUEST_FILE=${WORKING_DIR}"request-sents-n-ulfs.sql"
+ULF_SQL_REQUEST_FILE=${WORKING_DIR}"request-sents-n-ulfs-raw.sql"
 ULF_SQL_OUTPUT_FILE=${WORKING_DIR}"sid-sent-ulf-out.tsv"
 FINAL_ULF_OUTPUT_FILE="usid-isid-sent-ulf-out.tsv"
 TEMP_FINAL_ULF_FILE=${WORKING_DIR}${FINAL_ULF_OUTPUT_FILE}
 
 # TODO: double check these....
 INF_SQL_REQUEST_FILE=${WORKING_DIR}"request-sid-infs.sql"
-INF_SQL_OUTPUT_FILE=${WORKING_DIR}"sid-sent-ulf-out.tsv"
-FINAL_INF_OUTPUT_FILE="usid-isid-sent-ulf-out.tsv"
+INF_SQL_OUTPUT_FILE=${WORKING_DIR}"isid-inf-out-raw.tsv"
+FINAL_INF_OUTPUT_FILE="isid-inf-out.tsv"
 TEMP_FINAL_INF_FILE=${WORKING_DIR}${FINAL_INF_OUTPUT_FILE}
 
 python make-sql-requests.py ${DEFAULT_ULF_ANN_FILE} ${DEFAULT_INF_ANN_FILE} ${ULF_SQL_REQUEST_FILE} ${INF_SQL_REQUEST_FILE}
 mysql -h ebdb-instance2.cuainl7oscs9.us-east-2.rds.amazonaws.com -P 3306 -u gkim21 -p ebdb --password < ${ULF_SQL_REQUEST_FILE} > ${ULF_SQL_OUTPUT_FILE}
+mysql -h ebdb-instance2.cuainl7oscs9.us-east-2.rds.amazonaws.com -P 3306 -u gkim21 -p ebdb --password < ${INF_SQL_REQUEST_FILE} > ${INF_SQL_OUTPUT_FILE}
+
 python combine-and-format-ulf-anns.py ${ULF_SQL_OUTPUT_FILE} ${DATASET_JSON_FILE} ${TEMP_FINAL_ULF_FILE}
+python combine-and-format-inf-anns.py ${INF_SQL_OUTPUT_FILE} ${TEMP_FINAL_INF_FILE}
 
 cp ${TEMP_FINAL_ULF_FILE} ${FINAL_ULF_OUTPUT_FILE}
+cp ${TEMP_FINAL_INF_FILE} ${FINAL_INF_OUTPUT_FILE}
 
